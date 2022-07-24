437b21d379fca423703bf2ca58abc052696d65e8
aliahmed789489 <87897094+aliahmed789489@users.noreply.github.com>
2022-07-24 21:56:10 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var playBtn = document.querySelector(".play-btn")
+playBtn.addEventListener("click",function(){
+    var song = new Audio("images/crash.mp3");
+    if(song.pause){
+        song.play();
+        playBtn.src = "images/pause.png";
+        
+    }else{
+        song.pause();
+        playBtn.src = "images/play.png";
+    }
+		
+        
+
+
+})
